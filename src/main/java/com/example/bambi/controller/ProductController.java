873fc3498c35ee58f1cfadae8bc29e3376a774e8
Natalie Davies <210209859@aston.ac.uk>
2023-03-09 16:41:31 +0000

    minor spacing changes

--- src/main/java/com/example/bambi/controller/ProductController.java
+++ src/main/java/com/example/bambi/controller/ProductController.java
@@ -59,7 +59,6 @@ public class ProductController {
 
         int pageSize = 5;
         Page<Product> page = productService.findPaginated(keyword, pageNo, pageSize, sortField, sortDir);
-
         // Get all the products from the database
         List<Product> allProducts = productService.getAllProducts();
         // create a list to store products that are low in stock or out of stock
@@ -113,43 +112,6 @@ public class ProductController {
         model.addAttribute("keyword", keyword);
         return "products";
     }
-
-    //GET request to retrieve Stock info with all sizes
-    @GetMapping("/product/stock/{id}")
-    public String viewStock(@PathVariable(value = "id") long id, Model model) {
-
-        // Get the product details by ID
-        Product product = productService.getProductById(id);
-
-        //Get list of sizes
-        List<Size> sizes = sizeService.getSizesByProductId(product.getId());
-
-        //Calculate the total stock
-        int totalStockLevel = 0;
-        for (Size size : sizes){
-            totalStockLevel += size.getProductStock();
-        }
-
-        //Add the stock details to the model
-        model.addAttribute("product", product);
-        model.addAttribute("sizes", sizes);
-        model.addAttribute("totalStockLevel", totalStockLevel);
-
-        return "stock";
-    }
-
-    @PostMapping("product/stock/{sizeId}")
-    public String updateProductStock(@PathVariable Long sizeId, @RequestParam("stockToAdd") int stockToAdd) {
-        Size size = sizeService.getSizeById(sizeId);
-
-        int newStockLevel = size.getProductStock() + stockToAdd;
-        size.setProductStock(newStockLevel);
-        sizeService.saveSize(size);
-
-        return "redirect:/product/stock/" + size.getProduct().getId();
-
-    }
-
 
         //GET request to retrieve the Add Product Form
     @GetMapping("/products/new")
