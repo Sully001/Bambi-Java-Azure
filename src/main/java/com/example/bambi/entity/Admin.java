eed2ce1af60be2a6bf54184021db82790ba8ea3e
GIB225 <gib_@outlook.com>
2023-03-20 16:22:45 +0000

    -Implemented a login system for administrators, allowing them to securely access the admin panel. -Created a registration system that allows existing admins to add additional admins to the system -Improved the frontend design by incorporating Bootstrap,  user-friendly interface. -Added a navbar to provide easy access to all sections of the application. -Ensured that all pages are responsive
    NB : I am still working on the add and edit product page

---
+++ src/main/java/com/example/bambi/entity/Admin.java
@@ -0,0 +1,57 @@
+package com.example.bambi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.jetbrains.annotations.NotNull;
+
+@Entity
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+
+
+    // Constructor with parameters
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Default constructor
+    public Admin() {
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
