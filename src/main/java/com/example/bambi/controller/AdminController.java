2713e7d2c0879dca8789dd3f23f72a8bb55c6486
GIB225 <gib_@outlook.com>
2023-03-29 16:42:40 +0100

    -Frontend update -Password enforcement -Move charts to overview page

--- src/main/java/com/example/bambi/controller/AdminController.java
+++ src/main/java/com/example/bambi/controller/AdminController.java
@@ -2,20 +2,30 @@ package com.example.bambi.controller;
 
 
 import com.example.bambi.entity.Admin;
-import com.example.bambi.entity.Product;
+import com.example.bambi.entity.Order;
 import com.example.bambi.entity.Users;
 import com.example.bambi.repository.AdminRepository;
+import com.example.bambi.repository.OrderRepository;
 import com.example.bambi.repository.UsersRepository;
-import com.example.bambi.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import com.example.bambi.entity.Role;
+
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AdminController {
@@ -23,10 +33,9 @@ public class AdminController {
     private UsersRepository usersRepository;
     @Autowired
     private AdminRepository adminRepository;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
-    @Autowired
-    private ProductService productService;
 
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
@@ -35,26 +44,55 @@ public class AdminController {
     }
 
     @PostMapping("/register")
-    public String registerAdmin(@ModelAttribute Admin admin, Model model) {
-        admin.setPassword(passwordEncoder.encode(admin.getPassword()));
-        adminRepository.save(admin);        // Get a list of low stock products
-        List<Product> lowStockProducts = productService.getLowStockProducts();
-        model.addAttribute("lowStockProducts", lowStockProducts);
-        return "redirect:/home";
+    public String registerAdmin(@Valid @ModelAttribute Admin admin, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            List<String> errorMessages = bindingResult.getFieldErrors().stream()
+                    .map(fieldError -> fieldError.getDefaultMessage())
+                    .collect(Collectors.toList());
+            model.addAttribute("errorMessage", errorMessages);
+            return "register";
+        }
+        Optional<Admin> existingAdmin = adminRepository.findByUsername(admin.getUsername());
+        if (existingAdmin.isPresent()) {
+            model.addAttribute("errorMessage", "Username already exists! Please create another one.");
+            return "register";
+        } else {
+            admin.setPassword(passwordEncoder.encode(admin.getPassword()));
+            admin.setRole(Role.EDITOR);
+            adminRepository.save(admin);
+            model.addAttribute("successMessage", "Editor registered successfully!");
+            return "redirect:/manage_editors";
+        }
     }
+
     @GetMapping("/login")
     public String showLoginForm() {
         return "login";
     }
 
-    @GetMapping("/")
+    @GetMapping("/customer")
     public String getAllUsers(Model model) {
         List<Users> users = usersRepository.findAll();
-        // Get a list of low stock products
-        List<Product> lowStockProducts = productService.getLowStockProducts();
-        model.addAttribute("lowStockProducts", lowStockProducts);
         System.out.println("Retrieved " + users.size() + " orders from the database");
         model.addAttribute("users", users);
-        return "home";
+        return "customer";
     }
+
+    @GetMapping("/manage_editors")
+    public String manageEditors(Model model) {
+        List<Admin> editors = ((List<Admin>) adminRepository.findAll()).stream()
+                .filter(admin -> admin.getRole() == Role.EDITOR)
+                .collect(Collectors.toList());
+        model.addAttribute("editors", editors);
+        return "manage_editors";
+    }
+
+    @PostMapping("/delete_editor")
+    public String deleteEditor(@RequestParam Long id) {
+        adminRepository.deleteById(id);
+        return "redirect:/manage_editors";
+    }
+
+
+
 }
